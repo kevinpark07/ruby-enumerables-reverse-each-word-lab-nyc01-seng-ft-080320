d2fb7f273137c50bc6c4f7fe657e41497c72a3fd
Kevin Park <kevin.park07@yahoo.com>
2020-07-13 16:39:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 require 'pry' 
 
 def reverse_each_word(string)
- reverse_array = []
  new_array = string.split (" ")
  new_array.each {|string| 
- reverse_array << string.reverse
- reverse_array.join(" ")
+ string.reverse
  }
- 
+ array.join(" ")
+ }
+ new_array.join(" ")
 end
